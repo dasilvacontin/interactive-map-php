794faeac097b21f772674e72f80ed148ee820c9c
David da Silva <dasilvacontin@gmail.com>
2016-10-27 17:09:33 +0200

    Add client changes for english lang support

--- main.php
+++ main.php
@@ -12,6 +12,7 @@ function interactive_map_handler( $atts ) {
   ob_start();
 ?>
 <link rel="stylesheet" type="text/css" media="all" href="/wp-content/plugins/interactive_map/style.css">
+<!--div id="condicion"><?php $url= $_SERVER["REQUEST_URI"]; if ($url == "/en/hospitales-e-investigadores-en/"){ echo "URI: " . $url;} ?></div-->
 <div id="mapModule">
   <div id="mapWrapper" style="display: block;">
 		<img src="<?php echo $atts["logo"]; ?>" id="mapLogo" width="190">
